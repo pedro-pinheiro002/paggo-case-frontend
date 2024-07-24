dcad1980bc250c4d16c3e730a380b26a02474b6f
pedro-pinheiro002 <pedropinheiroborges2@gmail.com>
2024-07-24 02:58:03 -0300

    feat: add logic to save refresh token in cookies and JWT in memory

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,11 +1,14 @@
+import { removeCookies } from "@/actions/cookies";
+import { getCookies, setCookies } from "@/actions/cookies";
+import { getRefreshedTokens } from "@/http/api";
 import { User } from "@/types/types";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
-import { createContext, Dispatch, SetStateAction, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 interface AuthContextType {
   user: User | null;
-  handleLogIn: (idToken: string) => void;
+  handleLogIn: (idToken: string, refresh_token: string, user: User) => void;
   handleLogOut: () => void;
 }
 
@@ -17,11 +20,32 @@ export const AuthContext = createContext<AuthContextType>({
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
+  const [idToken, setIdToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!idToken) {
+      getCookies("refresh_token").then((refresh_token) => {
+        if (refresh_token) {
+          getRefreshedTokens(refresh_token)
+            .then((data) => {
+              setIdToken(data.tokens.id_token);
+              setUser(data.user);
+            })
+            .catch((error) => console.error(error));
+        }
+      });
+    }
+  }, []);
+
   function handleLogOut() {
     setUser(null);
+    setIdToken(null);
+    removeCookies("refresh_token");
   }
-  function handleLogIn(idToken: string) {
-    setUser(jwtDecode(idToken));
+  function handleLogIn(idToken: string, refresh_token: string, user: User) {
+    setUser(user);
+    setIdToken(idToken);
+    setCookies("refresh_token", refresh_token);
   }
   return (
     <GoogleOAuthProvider
