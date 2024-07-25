4da733eb5433d46361f80aa59a691f4039771446
pedro-pinheiro002 <pedropinheiroborges2@gmail.com>
2024-07-25 03:59:07 -0300

    fix: change user to isAuthenticated state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { Content } from "./Content";
 import { LogIn } from "./LogIn";
 
 export default function Home() {
-  const { user } = useAuthContext();
-  return <div className="p-36">{!user ? <LogIn /> : <Content />}</div>;
+  const { isAuthenticated } = useAuthContext();
+  return (
+    <div className="p-36">{!isAuthenticated ? <LogIn /> : <Content />}</div>
+  );
 }
